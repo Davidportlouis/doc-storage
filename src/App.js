dab1b97e730f5391565eb77f1611af2e48179f54
suryalk02 <dportlouis@gmail.com>
2019-09-27 10:54:38 +0530

    latest stable

---
+++ src/App.js
@@ -0,0 +1,123 @@
+//jshint esversion:8
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import web3 from './web3';
+import ipfs from './ipfs';
+import storehash from './storehash';
+// import { Button } from 'reactstrap';
+import './App.css';
+//eslint-disable-next-line
+import { read } from 'fs';
+import { async } from 'q';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ipfsHash: null,
+      buffer: '',
+      ethAddress: '',
+      blockNumber: '',
+      transactionHash: '',
+      gasUsed: '',
+      txRecipt: ''
+    };
+  }
+  captureFile = (event) => {
+    event.stopPropagation();
+    event.preventDefault();
+    const file = event.target.files[0]
+    let reader = new window.FileReader()
+    reader.readAsArrayBuffer(file)
+    reader.onloadend = () => {
+      this.convertToBuffer(reader);
+    }
+  }
+  convertToBuffer = async(reader) => {
+    const buffer = await Buffer.from(reader.result);
+    this.setState({ buffer });
+  }
+   onClick = async () => {
+    try {
+      this.setState({ blockNumber: "Loading...." });
+      this.setState({ gasUsed: "Loading...." });
+      await web3.eth.getTransactionReceipt(this.state.transactionHash, (err, txRecipt) => {
+        console.log(err, txRecipt);
+        this.setState(txRecipt);
+      });
+      await this.setState({ blockNumber: this.state.txRecipt.blockNumber });
+      await this.setState({ gasUsed: this.state.txRecipt.gasUsed });
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+  onSubmit = async (event) => {
+    event.preventDefault();
+    const accounts = await web3.eth.getAccounts();
+    console.log(accounts[0]);
+    const ethAddress = await storehash.options.address;
+    this.setState({ ethAddress });
+    await ipfs.add(this.state.buffer, (err, ipfsHash) => {
+      console.log(err, ipfsHash);
+      this.setState({ ipfsHash: ipfsHash[0].hash });
+      storehash.methods.setHash(this.state.ipfsHash).send({from: accounts[0]}, (error,transactionHash) => {
+        console.log(error,transactionHash);
+        this.setState({transactionHash});
+      });
+      });
+  }
+  render(){
+    return(
+      <div className="App">
+        <header className="App-header">
+          <h1>Tangler Technologies</h1>
+        </header>
+        <hr/>
+        <h3>Select Document</h3>
+        <form onSubmit={this.onSubmit}>
+          <input
+          className="input"
+            type="file"
+            onChange={this.captureFile}/>
+          <button type="submit">Upload</button>
+        </form>
+        <hr/>
+        <button onClick={this.onClick}>Get Transcation Details</button>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Tx Data</th>
+              <th>Value</th>
+              <th>View Link</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>IPFS Hash</td>
+              <td>{this.state.ipfsHash}</td>
+              <td><a href="https://gateway.ipfs.io/ipfs/">View File</a></td>
+            </tr>
+            <tr>
+              <td>Ethereum Address</td>
+              <td>{this.state.ethAddress}</td>
+            </tr>
+            <tr>
+              <td>Tx hash</td>
+              <td>{this.state.transactionHash}</td>
+            </tr>
+            <tr>
+              <td>Block Number</td>
+              <td>{this.state.blockNumber}</td>
+            </tr>
+            <tr>
+              <td>Gas Consumed</td>
+              <td>{this.state.gasUsed}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/storehash.js
@@ -0,0 +1,36 @@
+//jshint esversion:6
+
+import web3 from './web3';
+const address = '0xFa913601Fc2BDc8000Ab67Ed8e156875326a5dA8';
+const abi = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "x",
+				"type": "string"
+			}
+		],
+		"name": "setHash",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getHash",
+		"outputs": [
+			{
+				"name": "x",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+
+export default new web3.eth.Contract(abi, address);
